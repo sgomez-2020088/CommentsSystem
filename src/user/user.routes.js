183067575029636da49bb765843bd1b2e120c826
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-20 20:57:54 -0600

    se agrega ruta para get all user

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
-import { validateJwt } from '../../middlewares/validate.jwt.js'
-import { updateProfile} from '../user/user.controller.js'
+import { isAdmin, validateJwt } from '../../middlewares/validate.jwt.js'
+import { updateProfile, allUsers} from '../user/user.controller.js'
 import { updateUser } from '../../middlewares/validators.js'
 
 
 const api = Router()
 
 api.put('/update',[validateJwt,updateUser],updateProfile)
+
+api.get('/all',[validateJwt, isAdmin],allUsers)
 
 export default api
