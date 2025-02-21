8d9e4050e9846d29cc7a24b0a912a165dcace846
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-20 21:05:35 -0600

    se arregla error de git

--- src/category/category.routes.js
+++ src/category/category.routes.js
@@ -1,6 +1,11 @@
 import { Router } from 'express'
+<<<<<<< HEAD
 import { addCategory, updateCategoryValidator } from '../../middlewares/validators.js'
 import { createCategory, updateCategory, deleteCategory } from '../category/category.controller.js'
+=======
+import { addCategory  } from '../../middlewares/validators.js'
+import { createCategory, updateCategory, deleteCategory, getCategory } from '../category/category.controller.js'
+>>>>>>> f8ad0d9 (Se agrega ruta get en category)
 import {validateJwt,  isAdmin } from '../../middlewares/validate.jwt.js'
 
 const api = Router()
@@ -10,4 +15,6 @@ api.post('/add',[validateJwt,isAdmin,addCategory],createCategory)
 api.put('/update',[validateJwt, isAdmin, updateCategoryValidator],updateCategory)
 
 api.delete('/delete',[validateJwt,isAdmin],deleteCategory)
+
+api.get('/all',[validateJwt,isAdmin],getCategory)
 export default api
