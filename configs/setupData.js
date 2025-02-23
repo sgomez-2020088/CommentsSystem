bc62c1a0271c7e85a9e879a29f5c3b95647a6a3b
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-22 19:02:28 -0600

    Se agrega variable de entorno para password admin default

--- configs/setupData.js
+++ configs/setupData.js
@@ -8,7 +8,7 @@ export const createDefaultAdmin = async () => {
         const existingAdmin = await User.findOne({ email: adminEmail })
 
         if (!existingAdmin) {
-            const hashedPassword = await encrypt("Parmas./123")
+            const hashedPassword = await encrypt(process.env.DEFAULT_NAME)
             const adminUser = new User({
                 name: "Admin",
                 surname: "Default",
