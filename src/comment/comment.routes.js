cc6990873acd6fa490a30b725e346a73f47ae6ae
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-22 19:09:56 -0600

    se agregan rutas para comment

--- src/comment/comment.routes.js
+++ src/comment/comment.routes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express'
 import { validateJwt } from '../../middlewares/validate.jwt.js'
-import { addComment } from '../comment/comment.controller.js'
+import { addComment, editComment, deleteComment} from '../comment/comment.controller.js'
+import { addCommentValidator, updateCommentValidator, deleteCommentValidator } from '../../middlewares/validators.js'
 
 
 
 const api = Router()
 
-api.post('/add',[validateJwt],addComment)
+api.post('/add',[validateJwt,addCommentValidator],addComment)
+
+api.put('/edit',[validateJwt, updateCommentValidator],editComment)
+
+api.delete('/delete',[validateJwt, deleteCommentValidator],deleteComment)
 
 
 export default api
