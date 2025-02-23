4d2326787596f69c70e8c99c179845ffe70d4667
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-23 15:55:28 -0600

    se agregan rutas

--- src/comment/comment.routes.js
+++ src/comment/comment.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { validateJwt } from '../../middlewares/validate.jwt.js'
-import { addComment, editComment, deleteComment} from '../comment/comment.controller.js'
+import { addComment, editComment, deleteComment, getUserComments} from '../comment/comment.controller.js'
 import { addCommentValidator, updateCommentValidator, deleteCommentValidator } from '../../middlewares/validators.js'
 
 
@@ -12,6 +12,8 @@ api.post('/add',[validateJwt,addCommentValidator],addComment)
 api.put('/edit',[validateJwt, updateCommentValidator],editComment)
 
 api.delete('/delete',[validateJwt, deleteCommentValidator],deleteComment)
+
+api.get('/comments', [validateJwt], getUserComments)
 
 
 export default api
