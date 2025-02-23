8a805858b2607088d6f37b30c59139ff4ab8f07c
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-22 19:03:52 -0600

    se mejoran textos de output

--- middlewares/publication.validators.js
+++ middlewares/publication.validators.js
@@ -5,10 +5,10 @@ export const isPublicationAuthor = async (req, res, next) => {
         const { id } = req.body 
         const userId = req.user.id 
 
-        if (!id) return res.status(400).send({ message: 'Publication ID is required in body', success: false })
+        if (!id) return res.status(400).send({ message: ' ID publication is required', success: false })
         const publication = await Publication.findById(id)
         if (!publication) return res.status(404).send({ message: 'Publication not found', success: false })
-        if (publication.author.toString() !== userId.toString()) return res.status(403).send({ message: 'You are not the author of this publication', success: false })
+        if (publication.author.toString() !== userId.toString()) return res.status(403).send({ message: 'You are not the author', success: false })
 
         next() 
     } catch (err) {
