ceddf84470b52b26fc57fcd30a7b1143de13dd81
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-22 19:11:59 -0600

    se agregan rutas para publication

--- src/publication/publication.routes.js
+++ src/publication/publication.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { validateJwt } from '../../middlewares/validate.jwt.js'
-import { addPublication, updatePublication, deletePublication  } from './publication.controller.js'
+import { addPublication, updatePublication, deletePublication, getPublications } from './publication.controller.js'
 import { addPublicationValidator, updatePublicationValidator, deletePublicationValidator} from '../../middlewares/validators.js'
 import { isPublicationAuthor } from '../../middlewares/publication.validators.js'
 
@@ -13,5 +13,7 @@ api.post('/add',[validateJwt,addPublicationValidator],addPublication)
 api.put('/update',[validateJwt,isPublicationAuthor,updatePublicationValidator],updatePublication)
 
 api.delete('/delete',[validateJwt,isPublicationAuthor,deletePublicationValidator],deletePublication)
+
+api.get('/all',[validateJwt],getPublications)
 
 export default api
