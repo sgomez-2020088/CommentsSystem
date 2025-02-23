eb1ddfc78a1bf34a118445bbbe4b9b88205dbdc9
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-02-22 19:10:43 -0600

    se agrega crud publication

--- src/publication/publication.controller.js
+++ src/publication/publication.controller.js
@@ -1,6 +1,38 @@
 import Publication from '../publication/publication.model.js'
 import Category from '../category/category.model.js'
 
+
+
+export const getPublications = async (req, res) => {
+    try {
+        const { limit = 20, skip = 0 } = req.query;
+
+        const publications = await Publication.find({ status: true })
+            .skip(skip)
+            .limit(limit)
+            .populate('category', 'name -_id')
+            .populate('author', 'name username -_id')
+            .populate({
+                path: 'comments',
+                match: {status: true},
+                select: 'author content -_id',
+                populate: {
+                    path: 'author',
+                    select: 'name username -_id'
+                }
+            })
+
+        if (publications.length === 0) {
+            return res.status(404).send({ message: 'Publications not found', success: false });
+        }
+
+        return res.send({ message: 'All is right', publications, success: true });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send({ message: 'General error', success: false });
+    }
+}
+
 export const addPublication = async (req, res) => {
     try {
         const data = req.body
@@ -8,15 +40,13 @@ export const addPublication = async (req, res) => {
 
         const existCategory = await Category.findById(data.category)
         if (!existCategory) return res.status(404).send({ message: 'Invalid category', success: false })
-        
-
     
         const publication = new Publication({ ...data, author })
         await publication.save()
 
         const populatedPublication = await Publication.findById(publication._id)
-            .populate('author', 'name surname username') 
-            .populate('category', 'name')
+            .populate('author', 'name surname username -_id') 
+            .populate('category', 'name -_id')
 
 
         return res.send({ message: 'Publication created successfully', success: true, publication: populatedPublication})
@@ -30,15 +60,17 @@ export const addPublication = async (req, res) => {
 export const updatePublication = async (req, res) => {
     try {
         const { id, ...data } = req.body 
-        if (!id) return res.status(400).send({ message: 'Publication ID is required in body', success: false })
-        
+    
+        const publication = await Publication.findById(id)
+        if(publication.status === false) return res.status(404).send({message: 'This publication does not exist', success: false })
 
         const updatedPublication = await Publication.findByIdAndUpdate(
             id,
             { $set: data },
             { new: true, runValidators: true }
-        ).populate('author', 'name surname username email')
-        .populate('category', 'name description')
+        )
+        .populate('author', 'name surname username -_id')
+        .populate('category', 'name description -_id')
 
         if (!updatedPublication) return res.status(404).send({ message: 'Publication not found', success: false })
         
@@ -52,6 +84,8 @@ export const updatePublication = async (req, res) => {
 export const deletePublication = async (req, res) => {
     try {
         const id  = req.body.id
+        const publication = await Publication.findById(id)
+        if(publication.status === false) return res.status(404).send({message: 'This publication does not exist', success: false })
         const deletedPublication = await Publication.findByIdAndUpdate(
             id,
             {status: false},
@@ -65,3 +99,4 @@ export const deletePublication = async (req, res) => {
         return res.status(500).send({ message: 'General error', success: false })
     }
 }
+
